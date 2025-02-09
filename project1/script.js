5cdc2c3738cc478dac331f5d59937858e38940fc
nurik0001 <nurdauletusupov@gmail.com>
2025-02-09 14:16:47 +0500

    s

--- project1/script.js
+++ project1/script.js
@@ -15,3 +15,17 @@ function removeActiveClasses() {
     panel.classList.remove('active');
   })
 }
+ 
+def binary_seach(list, item){
+ low=0
+ high=len(list)-1;
+
+ while low <= high{
+  mid=(low = high)/2
+  guess=list[mid]
+   if guess =item (
+    return mid)
+    if guess 
+
+   }
+ }
